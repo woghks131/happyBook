506d9265372db4f683ad567efeaa108e53ce529f
rooney131 <pjh842697!>
2024-04-20 13:20:46 +0900

    Initial Commit

---
+++ HappyBook/HappyBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  HappyBookApp.swift
+//  HappyBook
+//
+//  Created by 박재환 on 4/20/24.
+//
+
+import SwiftUI
+
+@main
+struct HappyBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
