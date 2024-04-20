422183c485f17f4dcb52939d0efca2741c801320
rooney131 <pjh842697!>
2024-04-20 16:23:47 +0900

    2024.04.20 1. 로그인 화면 구현 중 - 카카오 로그인

---
+++ HappyBook/Source/User Interface/Login/KakaoSignInButton.swift
@@ -0,0 +1,18 @@
+//
+//  KakaoSignInButton.swift
+//  HappyBook
+//
+//  Created by 박재환 on 4/20/24.
+//
+
+import SwiftUI
+
+struct KakaoSignInButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    KakaoSignInButton()
+}
