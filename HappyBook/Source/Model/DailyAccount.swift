594cf65ce84eada4753b766e1b1ac54e8dbcf2d0
rooney131 <pjh842697!>
2024-04-29 23:09:16 +0900

    2024.04.29 - 메인화면 Row 데이터 표시 작업중

---
+++ HappyBook/Source/Model/DailyAccount.swift
@@ -0,0 +1,8 @@
+//
+//  DailyAccount.swift
+//  HappyBook
+//
+//  Created by 박재환 on 4/29/24.
+//
+
+import Foundation
---
+++ HappyBook/Source/Model/UserAccount.swift
@@ -0,0 +1,8 @@
+//
+//  UserAccount.swift
+//  HappyBook
+//
+//  Created by 박재환 on 4/29/24.
+//
+
+import Foundation
