64521702f2668df6271a98b2b8c8bbbd72aabe4d
rooney131 <pjh842697!>
2024-05-04 16:26:55 +0900

    2024.05.04 - 메모 화면 및 기능 완료 - 메모 수정 기능 예정

---
+++ HappyBook/Source/User Interface/Account/Memo/MemoRow.swift
@@ -0,0 +1,18 @@
+//
+//  MemoRow.swift
+//  HappyBook
+//
+//  Created by 박재환 on 5/4/24.
+//
+
+import SwiftUI
+
+struct MemoRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MemoRow()
+}
---
+++ HappyBook/Source/User Interface/Account/Memo/MemoView.swift
@@ -0,0 +1,18 @@
+//
+//  MemoView.swift
+//  HappyBook
+//
+//  Created by 박재환 on 5/4/24.
+//
+
+import SwiftUI
+
+struct MemoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MemoView()
+}
