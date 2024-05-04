0af8ad7aede5df5f320212cdf55b559f743ec7e8
rooney131 <pjh842697!>
2024-05-04 15:47:26 +0900

    2024.05.04 - 즐겨찾기 화면 및 기능 완료

---
+++ HappyBook/Source/User Interface/Account/FavoriteView/FavoriteRow.swift
@@ -0,0 +1,18 @@
+//
+//  FavoriteRow.swift
+//  HappyBook
+//
+//  Created by 박재환 on 5/3/24.
+//
+
+import SwiftUI
+
+struct FavoriteRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoriteRow()
+}
