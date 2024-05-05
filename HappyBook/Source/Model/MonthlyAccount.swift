858a0c58d8a5bf147575f9ddf31231450560652d
rooney131 <pjh842697!>
2024-05-05 19:14:59 +0900

    2024.05.05 - 월별 화면 및 기능 완료

---
+++ HappyBook/Source/Model/MonthlyAccount.swift
@@ -0,0 +1,8 @@
+//
+//  MonthlyAccount.swift
+//  HappyBook
+//
+//  Created by 박재환 on 5/5/24.
+//
+
+import Foundation
---
+++ HappyBook/Source/User Interface/Account/Monthly/MonthlyView.swift
@@ -0,0 +1,18 @@
+//
+//  MonthlyView.swift
+//  HappyBook
+//
+//  Created by 박재환 on 5/5/24.
+//
+
+import SwiftUI
+
+struct MonthlyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MonthlyView()
+}
